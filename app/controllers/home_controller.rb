5865724860dade3289f8eaa6c3847cd67bbd6bb0
mangochiman <mangochiman@gmail.com>
2016-02-25 12:02:25 +0200

    Modifying input event listeners - Enter key

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,9 @@
 class HomeController < ApplicationController
   def index
     session.delete(:dde_object) unless session[:dde_object].blank?
+
+    @settings = YAML.load_file("#{Rails.root}/config/dde_connection.yml")[Rails.env] rescue {}
+
     render :layout => false
   end
 end
