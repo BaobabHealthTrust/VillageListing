8eca5e99a171da5a4958c39c1b341a22faa7627c
Jacob Mziya <jacobmziya@gmail.com>
2016-10-31 15:39:16 +0200

    Added locale js

---
+++ app/assets/javascripts/locale.js
@@ -0,0 +1,17 @@
+/**
+ * Created by jacob on 10/31/16.
+ */
+
+var tstLocaleWords = {
+	"dzina loyamba": "First Name",
+	"dzina la bambo": "Last Name",
+	"mwamuna/mkazi": "Gender",
+	"mayina ofanan sanapezeke?": "Select Patient or Create New Patient",
+	"munthu watsopano ": "Create New Patient",
+	"maliza": "Finish",
+	"mkazi": "Female",
+	"mwamuna": "Male",
+	"f": "Female",
+	"m": "Male",
+	"sakhani": "Select Patient"
+};
