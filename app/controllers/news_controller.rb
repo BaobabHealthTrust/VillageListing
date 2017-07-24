c67c7f75d73e4a88ff87e4d4eb850dab21ce98ae
jmziya <jacobmziya@gmail.com>
2017-07-24 14:16:34 +0200

    Displaying auto retrieved news in a modal box.

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,8 +1,7 @@
 class NewsController < ApplicationController
+	skip_before_filter :check_if_logged_in # we want news to be accessible even without being logged in
+	
 	def index
-		if request.post?
-			raise params.inspect
-		end
 		render layout: false
 	end
 end
