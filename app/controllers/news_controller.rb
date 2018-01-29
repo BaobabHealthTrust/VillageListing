8c3d50111f29b3948405a1427ec325e8ecacc267
jmziya <jacobmziya@gmail.com>
2018-01-29 11:36:03 +0200

    Made News Application frontend working. 2% remaining. Needs testing first

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -2,6 +2,7 @@ class NewsController < ApplicationController
 	skip_before_filter :check_if_logged_in # we want news to be accessible even without being logged in
 	
 	def index
+		@news = News.all
 		render layout: false
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 	
 	get 'news/index'
+	post 'news/index' => 'home#index' # for redirecting from News(bad practise. to be redone)
 	
 	get 'main/index'
 	
