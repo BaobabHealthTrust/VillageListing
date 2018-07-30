8e2897b8d61b8922359ddd4a475ba5b36bac9221
jmziya <jacobmziya@gmail.com>
2018-07-30 15:24:19 +0200

    Worked on migration file for ahoy to allow json as string ans not json.

--- config/initializers/ahoy.rb
+++ config/initializers/ahoy.rb
@@ -2,10 +2,7 @@ class Ahoy::Store < Ahoy::DatabaseStore
 end
 
 # set to true for JavaScript tracking
-Ahoy.api = true
-
-# set protection to elements with no user-agents like bots
-#Ahoy.server_side_visits = false
+Ahoy.api = false
 
 # better user agent parsing
 Ahoy.user_agent_parser = :device_detector
